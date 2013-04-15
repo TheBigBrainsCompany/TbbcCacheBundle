38aab1638327e288d6b2151e31a5ce2a858eb8c8
Benjamin Dulau <benjamin.dulau@anonymation.com>
2013-04-15 18:03:55 +0200

    Added basic tests for annotation driver

---
+++ Tests/Metadata/Driver/AnnotationDriverTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Kitano\CacheBundle\Tests\Metadata\Driver;
+
+use Doctrine\Common\Annotations\AnnotationReader;
+use Kitano\CacheBundle\Metadata\Driver\AnnotationDriver;
+use Kitano\CacheBundle\Metadata\MethodMetadata;
+use Pel\Expression\Expression;
+
+class AnnotationDriverTest extends \PHPUnit_Framework_TestCase
+{
+    public function testLoadMetadataWithMethodCacheable()
+    {
+        $driver = new AnnotationDriver(new AnnotationReader());
+
+        $metadata = $driver->loadMetadataForClass(new \ReflectionClass('Kitano\CacheBundle\Tests\Metadata\Driver\Fixtures\CacheableService'));
+
+        $this->assertArrayHasKey('findFoo', $metadata->methodMetadata);
+
+        $this->assertEquals(array('foo_cache'), $metadata->methodMetadata['findFoo']->caches);
+        $this->assertEquals(MethodMetadata::CACHE_OPERATION_GET_OR_SET, $metadata->methodMetadata['findFoo']->cacheOperation);
+    }
+
+    public function testLoadMetadataWithMethodCacheEvict()
+    {
+        $driver = new AnnotationDriver(new AnnotationReader());
+
+        $metadata = $driver->loadMetadataForClass(new \ReflectionClass('Kitano\CacheBundle\Tests\Metadata\Driver\Fixtures\CacheableService'));
+
+        $this->assertArrayHasKey('saveFoo', $metadata->methodMetadata);
+
+        $this->assertEquals(array('foo_cache'), $metadata->methodMetadata['saveFoo']->caches);
+        $this->assertEquals(MethodMetadata::CACHE_OPERATION_EVICT, $metadata->methodMetadata['saveFoo']->cacheOperation);
+        $this->assertEquals(new Expression('#foo'), $metadata->methodMetadata['saveFoo']->key);
+    }
+}
+
+
---
+++ Tests/Metadata/Driver/Fixtures/CacheableService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Kitano\CacheBundle\Tests\Metadata\Driver\Fixtures;
+
+use Kitano\CacheBundle\Annotation\Cacheable;
+use Kitano\CacheBundle\Annotation\CacheEvict;
+
+class CacheableService
+{
+    /**
+     * @Cacheable(caches="foo_cache")
+     */
+    public function findFoo($bar, $baz)
+    {
+        $foo = 'foo';
+
+        return $foo;
+    }
+
+    /**
+     * @CacheEvict(caches="foo_cache", key="#foo")
+     */
+    public function saveFoo($foo)
+    {
+    }
+}
